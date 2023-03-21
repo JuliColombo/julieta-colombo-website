54592ab9d9f458a3f67b5134a9307f63753e3c98
Julieta Colombo <colombo.julieta.natalia@gmail.com>
2023-03-21 10:39:58 +0100

    Remove unused js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,12 @@
-$(function() {
+$(function () {
 
-    $('.navbar-toggle').click(function() {
+    $('.navbar-toggle').click(function () {
         $(this).toggleClass('act');
-            if($(this).hasClass('act')) {
-                $('.main-menu').addClass('act');
-            }
-            else {
-                $('.main-menu').removeClass('act');
-            }
-    });
-
-    //jQuery for page scrolling feature - requires jQuery Easing plugin
-    $(document).on('click', '.page-scroll a', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
-        }, 1000, 'easeInOutExpo');
-        event.preventDefault();
+        if ($(this).hasClass('act')) {
+            $('.main-menu').addClass('act');
+        } else {
+            $('.main-menu').removeClass('act');
+        }
     });
 
     // Highlight the top nav as scrolling occurs
@@ -25,34 +15,16 @@ $(function() {
         offset: 10
     });
 
-	/* Progress bar */
-    var $section = $('.section-articles');
-    function loadDaBars() {
-	    $('.progress .progress-bar').progressbar({
-	        transition_delay: 500
-	    });
-    }
-    
-    $(document).bind('scroll', function(ev) {
-        var scrollOffset = $(document).scrollTop();
-        var containerOffset = $section.offset().top - window.innerHeight;
-        if (scrollOffset > containerOffset) {
-            loadDaBars();
-            // unbind event not to load scrolsl again
-            $(document).unbind('scroll');
+
+    // Navigation overlay
+    skrollr.init({
+        forceHeight: false,
+        smoothScrolling: false,
+        mobileDeceleration: 0.004,
+        mobileCheck: function () {
+            //hack - forces mobile version to be off
+            return false;
         }
     });
 
-
-    // Navigation overlay
-    var s = skrollr.init({
-            forceHeight: false,
-            smoothScrolling: false,
-            mobileDeceleration: 0.004,
-            mobileCheck: function() {
-                //hack - forces mobile version to be off
-                return false;
-            }
-    });
-    
 });
